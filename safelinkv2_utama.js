a05b41b44c78afd6d6c19c866ebf70b125ee40e7
dewangga1999 <44418768+dewangga1999@users.noreply.github.com>
2018-11-18 19:43:00 +0700

    Update safelinkv2_utama.js

--- safelinkv2_utama.js
+++ safelinkv2_utama.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="10/stochastics-in-forex-trading.html"}
 if(x=="2"){xxx="10/volume-rate-of-change-indicator-and.html"}
 if(x=="3"){xxx="10/the-psychological-factors-of-forex.html"}
 if(x=="4"){xxx="10/three-new-ways-to-use-rsi-in-forex.html"}
-if(x=="5"){xxx="10/the-engulfing-entry-system.html"}a_to_ve[i].href="https://pulsagratisnih.blogspot.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="10/the-engulfing-entry-system.html"}a_to_ve[i].href="https://tetew.wibunolife.com/2018/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
